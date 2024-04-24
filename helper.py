0efde9753c6d059ad8c5b71b4c2362d792414e19
hrush7 <hrush7@gatech.edu>
2024-04-23 20:50:59 -0700

    Fixed feature transfer and added combined KD + Feature Transfer function

--- helper.py
+++ helper.py
@@ -3,32 +3,45 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.init as init
 from captum.attr import Saliency, IntegratedGradients, NoiseTunnel, DeepLift
 from captum.attr import visualization as viz
 from time import time
 # Check if GPU is available, and if not, use the CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train_first_stage(dataloader, student, teacher, criterion_hint, epochs, learning_rate):
+def train_first_stage(dataloader, student, teacher, criterion_hint, epochs, learning_rate, student_layer, teacher_layer):
     """ Train only up to the intermediate layer """
     start = time()
-    freeze_after_intermediate_layer2(student.features[4])
-    regressor = ConvolutionalRegressor().to(device)
-    optimizer = optim.Adam(student.parameters(), lr=learning_rate)
+    regressor = ConvolutionalRegressor2().to(device)
+    student.train()
+    teacher.eval()
+    regressor.train()
+    freeze_after_intermediate_layer2(student_layer)
+    
+    #optimizer = optim.Adam(student.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(
+    list(filter(lambda p: p.requires_grad, student.parameters())) + list(regressor.parameters()),
+    lr=learning_rate
+                        )
+    teacher_layer_hook= Hook(teacher_layer)
+    student_layer_hook= Hook(student_layer)
     for epoch in range(epochs):
         for inputs, _ in dataloader:
             inputs = inputs.to(device)
             optimizer.zero_grad()
-            teacher_layer_2_2 = Hook(teacher.layer2[2])
-            student_layer_seq_4 = Hook(student.features[4])
-            # intermediate outputs
-            student.forward(inputs)
-            with torch.no_grad():
-                teacher_middle = teacher.forward(inputs)
             
-            student_activations = student_layer_seq_4.output
-            teacher_activations = teacher_layer_2_2.output
+            #student_layer_hook = Hook(student_layer)
+            # intermediate outputs
+            logits= student(inputs)
+            with torch.no_grad():
+                teacher_middle = teacher(inputs)
+            
+            student_activations = student_layer_hook.output
+            teacher_activations = teacher_layer_hook.output
             teacher_activations_trans = regressor.forward(teacher_activations)
+            #print(f"teacher inter shape {teacher_activations_trans.shape}")
+            #print(f'student inter shape {student_activations.shape}')
             #  the hint loss
             loss = criterion_hint(student_activations, teacher_activations_trans)
             loss.backward()
@@ -39,7 +52,73 @@ def train_first_stage(dataloader, student, teacher, criterion_hint, epochs, lear
     end = time()
     unfreeze_all_layers(student)
     print(f"First Stage Training Time: {end - start}")
+    student_layer_hook.close()
+    teacher_layer_hook.close()
 
+def train_kd_intermediate_combined(teacher, student, train_loader, epochs, learning_rate, T, soft_target_loss_weight, 
+                          device, criterion_hint, student_layer, teacher_layer, early_stop = False):
+    print('Knowledge distillation training')
+    start = time()
+    early_stopper = EarlyStopper(patience=3, min_improv=0.1)
+    ce_loss_weight= 1 - soft_target_loss_weight
+    ce_loss = nn.CrossEntropyLoss().to(device)
+    regressor = ConvolutionalRegressor2().to(device)
+    optimizer = optim.Adam(
+    list(filter(lambda p: p.requires_grad, student.parameters())) + list(regressor.parameters()),
+    lr=learning_rate
+                        )
+    teacher_layer_hook = Hook(teacher_layer)
+    student_layer_hook = Hook(student_layer)
+    teacher.eval()  # Teacher set to evaluation mode
+    student.train() # Student to train mode
+    regressor.train()
+
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            
+            # Forward pass with the teacher model - do not save gradients here as we do not change the teacher's weights
+            with torch.no_grad():
+                teacher_logits = teacher(inputs)
+
+            
+            # Forward pass with the student model
+            student_logits = student(inputs)
+
+            student_activations = student_layer_hook.output
+            teacher_activations = teacher_layer_hook.output
+            teacher_activations_trans = regressor.forward(teacher_activations)
+            #  the hint loss
+            mse_loss = criterion_hint(student_activations, teacher_activations_trans)
+            #Soften the student logits by applying softmax first and log() second
+            soft_targets = nn.functional.softmax(teacher_logits / T, dim=-1)
+            soft_prob = nn.functional.log_softmax(student_logits / T, dim=-1)
+
+            # Calculate the soft target loss. Scaled by T**2 as suggested by the authors of the paper "Distilling the knowledge in a neural network"
+            soft_targets_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (T**2)
+
+            # Calculate the true label loss
+            label_loss = ce_loss(student_logits, labels)
+
+            # Weighted sum of the two losses
+            loss = soft_target_loss_weight * soft_targets_loss + ce_loss_weight * label_loss + mse_loss
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss / len(train_loader)}")
+        if early_stop and early_stopper.check_loss(running_loss):
+          break
+    end = time()
+    runtime = end - start
+    print(f"Training Time: {runtime:.3f}")
+    student_layer_hook.close()
+    teacher_layer_hook.close()
 
 def freeze_after_intermediate_layer2( layer):
         layer.requires_grad = False
@@ -53,22 +132,68 @@ def unfreeze_all_layers(model):
 class Hook():
     def __init__(self, layer):
         self.hook = layer.register_forward_hook(self.hook_fn)
-       
+        print(f"Hook Set: {layer}")
     def hook_fn(self, module, input, output):
         self.input = input
         self.output = output
     def close(self):
         self.hook.remove()
 
+
+
 class ConvolutionalRegressor(nn.Module):
     def __init__(self):
         super(ConvolutionalRegressor, self).__init__()
-        # This regressor maps 32 channels to 16 channels
-        self.regressor = nn.Conv2d(32, 16, kernel_size=(1, 1))  # 1x1 convolution
+        # This regressor maps 1024 channels to 64 channels
+        self.regressor = nn.Conv2d(1024, 128, kernel_size=(1, 1))  # 1x1 convolution
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((4, 4))
+
+    def forward(self, x):
+        x = self.regressor(x)
+        x = self.adaptive_pool(x)
+        return x    
+    
+
+class ConvolutionalRegressor2(nn.Module):
+    def __init__(self, in_channels=1024, out_channels=128, kernel_size=4, stride=2, padding=1):
+        super(ConvolutionalRegressor2, self).__init__()
+        self.regressor = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, 
+                               stride=stride, padding=padding),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+        self.init_weights()
+        
+    def init_weights(self):
+        init.kaiming_uniform_(self.regressor[0].weight, mode='fan_in', nonlinearity='relu')
+        if self.regressor[0].bias is not None:
+            init.constant_(self.regressor[0].bias, 0)
 
     def forward(self, x):
         return self.regressor(x)
+
     
 def hint_loss(student_activations, teacher_activations):
     return torch.nn.functional.mse_loss(student_activations, teacher_activations).to(device)
 
+class EarlyStopper:
+    def __init__(self, patience=3, min_improv=0.01):
+        self.patience = patience
+        self.min_improv = min_improv
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+
+    def check_loss(self, validation_loss):
+      # if validation loss improve by at least min_improv percentage, then
+      # set min to current loss and reset the counter
+        if validation_loss <  (self.min_validation_loss * (1 - self.min_improv)):
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+      # else if validation loss exceeds previous loss by the min_improv percentage,
+      # start counter until hit patience
+        elif validation_loss > (self.min_validation_loss * (1 + self.min_improv)):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
